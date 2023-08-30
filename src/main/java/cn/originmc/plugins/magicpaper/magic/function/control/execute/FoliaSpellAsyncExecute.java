3d13ffbdfbaf7b25a27b498de98c31c1f881ca57
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-08-30 20:41:37 +0800

    v 1.0.2b1 folia支持准备

---
+++ src/main/java/cn/originmc/plugins/magicpaper/magic/function/control/execute/FoliaSpellAsyncExecute.java
@@ -0,0 +1,25 @@
+package cn.originmc.plugins.magicpaper.magic.function.control.execute;
+
+import cn.origincraft.magic.function.NormalFunction;
+import cn.origincraft.magic.object.SpellContext;
+import dev.rgbmc.expression.functions.FunctionResult;
+
+import java.util.List;
+
+public class FoliaSpellAsyncExecute extends NormalFunction {
+
+    @Override
+    public FunctionResult whenFunctionCalled(SpellContext spellContext, List<FunctionResult> list) {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
