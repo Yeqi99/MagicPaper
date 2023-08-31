96e38cb6293d1ba07c83e4c0053218fd642f7343
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-08-31 20:56:49 +0800

    v 1.0.11 更新依赖

--- src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
+++ src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
@@ -80,7 +80,7 @@ public final class MagicPaper extends JavaPlugin {
         context=new NormalContext();
     }
     public static String getVersion(){
-        return "1.0.10";
+        return "1.0.11";
     }
     public static String getLang(){
         return getInstance().getConfig().getString("lang");
