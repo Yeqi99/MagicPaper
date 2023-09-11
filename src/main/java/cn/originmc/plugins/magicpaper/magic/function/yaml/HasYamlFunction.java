1d162241bfc0a9dee5754c6f862e7f16e4e2fc13
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-09-11 18:27:19 +0800

    v 1.0.29 更新预备

--- src/main/java/cn/originmc/plugins/magicpaper/magic/function/yaml/HasYamlFunction.java
+++ src/main/java/cn/originmc/plugins/magicpaper/magic/function/yaml/HasYamlFunction.java
@@ -1,4 +1,25 @@
 package cn.originmc.plugins.magicpaper.magic.function.yaml;
 
-public class HasYamlFunction {
+import cn.origincraft.magic.function.NormalFunction;
+import cn.origincraft.magic.object.SpellContext;
+import dev.rgbmc.expression.functions.FunctionResult;
+
+import java.util.List;
+
+public class HasYamlFunction extends NormalFunction{
+
+    @Override
+    public FunctionResult whenFunctionCalled(SpellContext spellContext, List<FunctionResult> list) {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
 }
