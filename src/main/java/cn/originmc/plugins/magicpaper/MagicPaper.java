fba3ee5d097677b19f2c2835e6fe50910bb82104
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-09-18 18:13:21 +0800

    v 1.0.36 修复附魔名称

--- src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
+++ src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
@@ -104,7 +104,7 @@ public final class MagicPaper extends JavaPlugin {
         context=new NormalContext();
     }
     public static String getVersion(){
-        return "1.0.35";
+        return "1.0.36";
     }
     public static String getLang(){
         return getInstance().getConfig().getString("lang");
