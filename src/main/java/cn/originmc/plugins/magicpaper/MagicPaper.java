5d1a83f53c66cf3e67aba5ba74e983bc9ce8e73d
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-09-25 16:31:23 +0800

    v 1.1.2 优化体验

--- src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
+++ src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
@@ -120,7 +120,7 @@ public final class MagicPaper extends JavaPlugin {
         context=new NormalContext();
     }
     public static String getVersion(){
-        return "1.1.1";
+        return "1.1.2";
     }
     public static String getLang(){
         return getInstance().getConfig().getString("lang");
