382192ca42363762dd2494a219265e02c2958ae0
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-10-18 19:20:12 +0800

    v1.2.2 语义 playerCloseInv() 关闭正在打开的背包 playerHasPermission() 使用luckperms检查玩家权限
    触发器
    PlayerBreakTrigger
    PlayerPlaceTrigger
    EntityDamageTrigger
    
    增加魔咒执行返回papi变量支持
    修复LuckPermsHook错误问题

--- src/main/java/cn/originmc/plugins/magicpaper/hook/LuckPermsHook.java
+++ src/main/java/cn/originmc/plugins/magicpaper/hook/LuckPermsHook.java
@@ -3,6 +3,7 @@ package cn.originmc.plugins.magicpaper.hook;
 import cn.originmc.plugins.magicpaper.MagicPaper;
 import net.luckperms.api.LuckPerms;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class LuckPermsHook {
     public static boolean status = false;
@@ -15,6 +16,8 @@ public class LuckPermsHook {
     public static boolean hook() {
         if (Bukkit.getPluginManager().getPlugin(getName()) != null) {
             MagicPaper.getSender().sendToLogger("&a成功挂钩"+getName()+"插件");
+            RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+            api = provider.getProvider();
             status=true;
             return true;
         } else {
