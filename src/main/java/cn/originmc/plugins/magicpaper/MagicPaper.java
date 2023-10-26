3e8f175fde9abd991e33a02b11544f2120562a20
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-10-26 21:35:40 +0800

    v1.4.1(2023/10/26)
    新增语义
    guiOpen()
    	打开gui
    guiPreviousPage()
    	打开上一页gui
    guiNextPage()
    	打开下一页gui
    guiDataClear()
    	清除gui数据
    
    数据源按钮更新预备

--- src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
+++ src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
@@ -173,7 +173,7 @@ public final class MagicPaper extends JavaPlugin {
     }
 
     public static String getVersion() {
-        return "1.4.0";
+        return "1.4.1";
     }
 
     public static String getLang() {
