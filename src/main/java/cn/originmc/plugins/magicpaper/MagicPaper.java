61eb62f81b634acd7f2c4b6738074968755c71a1
Yeqi99 <72844744+Yeqi99@users.noreply.github.com>
2023-10-28 19:37:30 +0800

    v1.5.1(2023/10/28)
    文本词条可配置化

--- src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
+++ src/main/java/cn/originmc/plugins/magicpaper/MagicPaper.java
@@ -172,7 +172,7 @@ public final class MagicPaper extends JavaPlugin {
     }
 
     public static String getVersion() {
-        return "1.5.0";
+        return "1.5.1";
     }
 
     public static String getLang() {
